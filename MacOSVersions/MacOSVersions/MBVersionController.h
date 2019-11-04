32986b4cb1db9876c06106dfeb177283a7ba5104
Mitchell Budge <mitchellbudge@gmail.com>
2019-11-04 11:13:52 -0700

    Create model controller file

---
+++ MacOSVersions/MacOSVersions/MBVersionController.h
@@ -0,0 +1,17 @@
+//
+//  MBVersionController.h
+//  MacOSVersions
+//
+//  Created by Mitchell Budge on 11/4/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MBVersionController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MacOSVersions/MacOSVersions/MBVersionController.m
@@ -0,0 +1,13 @@
+//
+//  MBVersionController.m
+//  MacOSVersions
+//
+//  Created by Mitchell Budge on 11/4/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+#import "MBVersionController.h"
+
+@implementation MBVersionController
+
+@end
